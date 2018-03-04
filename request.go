36bb86f4a9573ae0fd8a29b9d87018bf332dfe2c
Arien Malec <arien.malec@gmail.com>
2018-03-04 12:43:40 -0800

    Initial commit

---
+++ request.go
@@ -0,0 +1,60 @@
+package alexa
+
+// request
+
+// Request is an Alexa skill request
+// see https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html#request-format
+type Request struct {
+	Version string  `json:"version"`
+	Session Session `json:"session"`
+	Body    ReqBody `json:"request"`
+	Context Context `json:"context"`
+}
+
+// Session represents the Alexa skill session
+type Session struct {
+	New         bool   `json:"new"`
+	SessionID   string `json:"sessionId"`
+	Application struct {
+		ApplicationID string `json:"applicationId"`
+	} `json:"application"`
+	Attributes map[string]interface{} `json:"attributes"`
+	User       struct {
+		UserID      string `json:"userId"`
+		AccessToken string `json:"accessToken,omitempty"`
+	} `json:"user"`
+}
+
+// Context represents the Alexa skill request context
+type Context struct {
+	System struct {
+		APIAccessToken string `json:"apiAccessToken"`
+		Device         struct {
+			DeviceID string `json:"deviceId,omitempty"`
+		} `json:"device,omitempty"`
+		Application struct {
+			ApplicationID string `json:"applicationId,omitempty"`
+		} `json:"application,omitempty"`
+	} `json:"System,omitempty"`
+}
+
+// ReqBody is the actual request information
+type ReqBody struct {
+	Type      string `json:"type"`
+	RequestID string `json:"requestId"`
+	Timestamp string `json:"timestamp"`
+	Intent    Intent `json:"intent,omitempty"`
+	Reason    string `json:"reason,omitempty"`
+}
+
+// Intent is the Alexa skill intent
+type Intent struct {
+	Name  string          `json:"name"`
+	Slots map[string]Slot `json:"slots"`
+}
+
+// Slot is an Alexa skill slot
+type Slot struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
---
+++ response.go
@@ -0,0 +1,59 @@
+package alexa
+
+//NewSimpleResponse builds a session response
+func NewSimpleResponse(title string, text string) Response {
+	r := Response{
+		Version: "1.0",
+		Body: ResBody{
+			OutputSpeech: &Payload{
+				Type: "PlainText",
+				Text: text,
+			},
+			Card: &Payload{
+				Type:    "Simple",
+				Title:   title,
+				Content: text,
+			},
+			ShouldEndSession: true,
+		},
+	}
+	return r
+}
+
+// Response Types
+
+// Response is the response back to the Alexa speech service
+type Response struct {
+	Version           string                 `json:"version"`
+	SessionAttributes map[string]interface{} `json:"sessionAttributes,omitempty"`
+	Body              ResBody                `json:"response"`
+}
+
+// ResBody is the actual body of the response
+type ResBody struct {
+	OutputSpeech     *Payload  `json:"outputSpeech,omitempty"`
+	Card             *Payload  `json:"card,omitempty"`
+	Reprompt         *Reprompt `json:"reprompt,omitempty"`
+	ShouldEndSession bool      `json:"shouldEndSession"`
+}
+
+// Reprompt is imformation
+type Reprompt struct {
+	OutputSpeech Payload `json:"outputSpeech,omitempty"`
+}
+
+// Image ...
+type Image struct {
+	SmallImageURL string `json:"smallImageUrl,omitempty"`
+	LargeImageURL string `json:"largeImageUrl,omitempty"`
+}
+
+// Payload ...
+type Payload struct {
+	Type    string `json:"type,omitempty"`
+	Title   string `json:"title,omitempty"`
+	Text    string `json:"text,omitempty"`
+	SSML    string `json:"ssml,omitempty"`
+	Content string `json:"content,omitempty"`
+	Image   Image  `json:"image,omitempty"`
+}
