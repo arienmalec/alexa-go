7e82b59da5c01097812cd734ed83a5fcee499756
Guilherme Ferreira <guilhermeaferreira_t@yahoo.com.br>
2019-09-19 19:52:26 +0200

    adding "confirmationStatus"

--- request.go
+++ request.go
@@ -110,6 +110,7 @@ type ReqBody struct {
 // Intent is the Alexa skill intent
 type Intent struct {
 	Name  string          `json:"name"`
+	ConfirmationStatus string `json:"confirmationStatus"`
 	Slots map[string]Slot `json:"slots"`
 }
 
