ff8d6313956f8582bdf34f8040ae1df4389ef6ca
mrmarss <nmaresco@bu.edu>
2020-08-22 11:36:37 -0400

    adding response extensions

--- response.go
+++ response.go
@@ -1,5 +1,66 @@
 package alexa
 
+import (
+	"strings"
+)
+
+// String parse utility.
+func ParseString(text string) string {
+	text = strings.ToLower(text)
+	text = strings.Replace(text, "&", "and", -1)
+	text = strings.Replace(text, "+", "plus", -1)
+	text = strings.Replace(text, "@", "at", -1)
+	text = strings.Replace(text, "w/", "with", -1)
+	text = strings.Replace(text, "in.", "inches", -1)
+	text = strings.Replace(text, "s/h", "shipping and handling", -1)
+	text = strings.Replace(text, " ac ", " after coupon ", -1)
+	text = strings.Replace(text, "fs", "free shipping", -1)
+	text = strings.Replace(text, "f/s", "free shipping", -1)
+	text = strings.Replace(text, "-", "", -1)
+	text = strings.Replace(text, "™", "", -1)
+	text = strings.Replace(text, "  ", " ", -1)
+	return text
+}
+
+// Inject new text in builder
+func (builder *SSMLBuilder) Say(text string) {
+	text = ParseString(text)
+	builder.SSML = append(builder.SSML, SSML{text: text})
+}
+
+// Inject pauses in builder
+func (builder *SSMLBuilder) Pause(pause string) {
+	builder.SSML = append(builder.SSML, SSML{pause: pause})
+}
+
+// Build SSML response text
+func (builder *SSMLBuilder) Build() string {
+	var response string
+	for index, ssml := range builder.SSML {
+		if ssml.text != "" {
+			response += ssml.text + " "
+		} else if ssml.pause != "" && index != len(builder.SSML)-1 {
+			response += "<break time='" + ssml.pause + "ms'/> "
+		}
+	}
+	return "<speak>" + response + "</speak>"
+}
+
+//NewSSMLResponse needs builder for more advanced responses
+func NewSSMLResponse(title string, text string) Response {
+	r := Response{
+		Version: "1.0",
+		Body: ResBody{
+			OutputSpeech: &Payload{
+				Type: "SSML",
+				SSML: text,
+			},
+			ShouldEndSession: true,
+		},
+	}
+	return r
+}
+
 //NewSimpleResponse builds a session response
 func NewSimpleResponse(title string, text string) Response {
 	r := Response{
@@ -31,11 +92,11 @@ type Response struct {
 
 // ResBody is the actual body of the response
 type ResBody struct {
-	OutputSpeech     *Payload  `json:"outputSpeech,omitempty"`
-	Card             *Payload  `json:"card,omitempty"`
-	Reprompt         *Reprompt `json:"reprompt,omitempty"`
+	OutputSpeech     *Payload     `json:"outputSpeech,omitempty"`
+	Card             *Payload     `json:"card,omitempty"`
+	Reprompt         *Reprompt    `json:"reprompt,omitempty"`
 	Directives       []Directives `json:"directives,omitempty"`
-	ShouldEndSession bool      `json:"shouldEndSession"`
+	ShouldEndSession bool         `json:"shouldEndSession"`
 }
 
 // Reprompt is imformation
@@ -79,4 +140,15 @@ type Payload struct {
 	SSML    string `json:"ssml,omitempty"`
 	Content string `json:"content,omitempty"`
 	Image   Image  `json:"image,omitempty"`
+}
+
+// SSML ...
+type SSML struct {
+	text  string
+	pause string
+}
+
+// SSMLBuilder ...
+type SSMLBuilder struct {
+	SSML []SSML
 }
